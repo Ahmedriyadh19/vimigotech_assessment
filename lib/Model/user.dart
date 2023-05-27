ef7b47ec1d88492e96f217a2470632bb3631e849
AHMED RIYADH MOHAMMED AHMED <alahmedriyadh1993@gmail.com>
2023-05-27 10:40:10 +0800

    Update v6.7

--- lib/Model/user.dart
+++ lib/Model/user.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:intl/intl.dart';
+import 'package:vimigotech_assessment/Services/time_ago.dart';
+
 class User {
   final String user;
   final String phone;
@@ -20,4 +24,7 @@ class User {
       'check-in': checkIn.toIso8601String(),
     };
   }
+
+  String toStringPrint({required bool format}) =>
+      'Attendance:\nName: $user\nPhone Number: $phone\nCheck In: ${format ? CountTimeAgo(checkIn).timeAgo() : DateFormat('dd MMM yyyy, h:mm a').format(checkIn)})';
 }
--- lib/View/Pages/selected_user_page.dart
+++ lib/View/Pages/selected_user_page.dart
@@ -11,15 +11,23 @@ import 'package:vimigotech_assessment/Model/user.dart';
 import 'package:vimigotech_assessment/Services/time_ago.dart';
 import 'package:vimigotech_assessment/View/Components/background.dart';
 
-class SelectedUserPage extends StatelessWidget {
+class SelectedUserPage extends StatefulWidget {
   final User userDisplayed;
   final bool isTimeAgoActive;
-  final ScreenshotController screenshotController = ScreenshotController();
-  SelectedUserPage({
+
+  const SelectedUserPage({
     Key? key,
     required this.isTimeAgoActive,
     required this.userDisplayed,
   }) : super(key: key);
+
+  @override
+  State<SelectedUserPage> createState() => _SelectedUserPageState();
+}
+
+class _SelectedUserPageState extends State<SelectedUserPage> {
+  final ScreenshotController screenshotController = ScreenshotController();
+  bool _isShot = false;
 
   Column body({required BuildContext ctx}) {
     return Column(
@@ -46,7 +54,7 @@ class SelectedUserPage extends StatelessWidget {
       child: CircleAvatar(
           radius: 60,
           child: Text(
-            userDisplayed.user[0].toUpperCase(),
+            widget.userDisplayed.user[0].toUpperCase(),
             style: const TextStyle(fontSize: 60),
           )),
     );
@@ -54,20 +62,23 @@ class SelectedUserPage extends StatelessWidget {
 
   Container dataDisplay() {
     return Container(
+        decoration: BoxDecoration(color: _isShot ? Colors.white : Colors.transparent, borderRadius: BorderRadius.circular(12.0)),
         margin: const EdgeInsets.all(18),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             pic(),
-            oneRow(iconData: Icons.person, txt: userDisplayed.user),
-            oneRow(iconData: Icons.phone_android_rounded, txt: userDisplayed.phone),
+            oneRow(iconData: Icons.person, txt: widget.userDisplayed.user),
+            oneRow(iconData: Icons.phone_android_rounded, txt: widget.userDisplayed.phone),
             oneRow(
               iconData: Icons.calendar_month_rounded,
-              txt: isTimeAgoActive ? CountTimeAgo(userDisplayed.checkIn).timeAgo() : DateFormat('dd MMM yyyy, h:mm a').format(userDisplayed.checkIn),
+              txt: widget.isTimeAgoActive
+                  ? CountTimeAgo(widget.userDisplayed.checkIn).timeAgo()
+                  : DateFormat('dd MMM yyyy, h:mm a').format(widget.userDisplayed.checkIn),
             )
           ],
         ).asGlass(
-          tintColor: Colors.black,
+          tintColor: _isShot ? Colors.white : Colors.black,
           clipBorderRadius: BorderRadius.circular(12.0),
         ));
   }
@@ -76,9 +87,15 @@ class SelectedUserPage extends StatelessWidget {
     return IconButton(
       icon: const Icon(Icons.share_rounded),
       onPressed: () async {
+        setState(() {
+          _isShot = true;
+        });
         final b = await screenshotController.capture();
         await saveImage(bytes: b);
         await loadImage();
+        setState(() {
+          _isShot = false;
+        });
       },
     ).asGlass(
       tintColor: Colors.black,
@@ -97,7 +114,8 @@ class SelectedUserPage extends StatelessWidget {
     final file = XFile('${appStorages.path}/image.png');
 
     if (file.path.isNotEmpty) {
-      Share.shareXFiles([file], text: userDisplayed.toString(), subject: 'jjfjfjf');
+      debugPrint(widget.userDisplayed.toString());
+      Share.shareXFiles([file], text: widget.userDisplayed.toStringPrint(format: widget.isTimeAgoActive));
     }
   }
 
